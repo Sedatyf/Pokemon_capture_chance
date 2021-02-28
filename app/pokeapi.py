1f6ec82b5d2c7986682a9826c321408c980cc603
Sedatyf <gaxel@hotmail.fr>
2021-02-28 13:16:51 +0100

    Delete an  useless str() conversion in get_capture_rate()

--- app/pokeapi.py
+++ app/pokeapi.py
@@ -6,7 +6,7 @@ def get_capture_rate(pokemon):
     page = requests.get(f"https://www.pokepedia.fr/{pokemon.lower()}")
     html_tree = html.fromstring(page.content)
     etree_object = html_tree.xpath("(//td[@colspan='3'])[14]/text()")
-    capture_rate = int(str(etree_object[0]))
+    capture_rate = int(etree_object[0])
 
     return capture_rate
 
