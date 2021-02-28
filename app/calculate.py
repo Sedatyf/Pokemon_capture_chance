a62a0ed768ffb9dbd201032cd4be7fcfe3113d1c
Sedatyf <gaxel@hotmail.fr>
2021-02-28 11:44:20 +0100

    Add calculate max hp and current hp

--- app/calculate.py
+++ app/calculate.py
@@ -6,6 +6,16 @@ def calculate_a(actual_hp, max_hp, rate, ball, status):
     return a
 
 
+def calculate_max_hp(base_hp, level):
+    max_hp = (((2 * base_hp + 31 + (127 / 4)) * level) / 100) + level + 10
+    return round(max_hp, 0)
+
+
+def calculate_current_hp(current_percent, max_hp):
+    current_hp = (current_percent / 100) * max_hp
+    return current_hp
+
+
 def calculate_capture_chance(a):
     y = 1048560 / (math.sqrt(math.sqrt(16711680 / a)))
     p = round((y / 65536) ** 4, 4)
