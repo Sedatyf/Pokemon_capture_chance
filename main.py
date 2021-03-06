28358d527d0d37d15ec595428ffe1112c7bf1764
Sedatyf <gaxel@hotmail.fr>
2021-03-06 02:32:39 +0100

    Modify main : transform to only gui

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
+from PySide2 import QtWidgets
+
 import package.pokeapi as pokeapi
 import package.calculate as calculate
+import package.gui as gui
 
-pokemon = pokeapi.get_pokemon()
-level = pokeapi.get_level()
-percent_hp = pokeapi.get_current_hp()
-ball = pokeapi.get_bonus_ball()
-status = pokeapi.get_bonus_status()
+app = QtWidgets.QApplication([])
+window = gui.MainWindow()
+window.show()
+app.exec_()
 
-capture_rate = pokeapi.get_capture_rate(pokemon)
-base_hp = pokeapi.get_base_hp(pokemon)
-
-max_hp = calculate.calculate_max_hp(base_hp, level)
-current_hp = calculate.calculate_current_hp(percent_hp, max_hp)
-a = calculate.calculate_a(current_hp, max_hp, capture_rate, ball, status)
-
-if a >= 255:
-    print(f"You have a 100% chance to capture {pokemon}!")
-else:
-    p = calculate.calculate_capture_chance(a)
-    print(f"You have, approximately, a {p * 100}% chance to capture {pokemon} per ball with the chosen ball.")
+#if a >= 255:
+#    print(f"You have a 100% chance to capture {pokemon}!")
+#else:
+#    p = calculate.calculate_capture_chance(a)
+#    print(f"You have, approximately, a {p * 100}% chance to capture {pokemon} per ball with the chosen ball.")
