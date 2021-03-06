6a651e22d0064d37e4d1a424393d5e89517fa358
Sedatyf <gaxel@hotmail.fr>
2021-03-06 11:47:43 +0100

    Delete unused import and comments in main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 from PySide2 import QtWidgets
-
-import package.pokeapi as pokeapi
-import package.calculate as calculate
 import package.gui as gui
 
 app = QtWidgets.QApplication([])
 window = gui.MainWindow()
 window.show()
 app.exec_()
-
-#if a >= 255:
-#    print(f"You have a 100% chance to capture {pokemon}!")
-#else:
-#    p = calculate.calculate_capture_chance(a)
-#    print(f"You have, approximately, a {p * 100}% chance to capture {pokemon} per ball with the chosen ball.")
